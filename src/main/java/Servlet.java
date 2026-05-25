8b9d477c0ee21a250a4a4baf3baebd41262db66b
Motilal Gupta <motilalgupta943071@gmail.com>
2026-05-25 17:03:18 +0530

    Setup servlet web application

---
+++ src/main/java/Servlet.java
@@ -0,0 +1,16 @@
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h2>Hello from Motilal Gupta!</h2>");
+    }
+}
